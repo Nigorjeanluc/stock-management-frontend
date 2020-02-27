e84ef0491300c1db01c426d9408eb89fdf032df3
blaise82 <nigorjeanluc@gmail.com>
2020-02-27 18:42:32 +0200

    Ensure that user can delete an item

---
+++ src/redux/item/itemReducer.js
@@ -0,0 +1,40 @@
+import { ADD_ITEM } from './itemTypes'
+import { DELETE_ITEM } from './itemTypes'
+
+const initialState = {
+  items: [{
+    name: 'Car',
+    price: '100000 Rwf'
+  }, {
+    name: 'Shoes',
+    price: '10000 Rwf'
+  }, {
+    name: 'Tire',
+    price: '2000 Rwf'
+  }, {
+    name: 'Shirt',
+    price: '5000 Rwf'
+  }]
+}
+
+const itemReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case ADD_ITEM:
+      return {
+        ...state,
+        items: state.items.push(action.payload)
+      }
+    
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter((item, index) => {
+          return index !== action.payload
+        })
+      }
+    
+    default: return state
+  }
+}
+
+export default itemReducer
